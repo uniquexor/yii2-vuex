632d79190d0a621099472f156efb39a793d636df
Algirdas Gurevicius <a.gurevicius@gmail.com>
2022-02-21 20:15:21 +0200

    bugfix

--- src/commands/VueAssetsController.php
+++ src/commands/VueAssetsController.php
@@ -15,12 +15,12 @@
          */
         public string $js_model_template_path = __DIR__ . DIRECTORY_SEPARATOR . '__vue_model_template.js';
 
-        protected function generateBody( string $attribute_name, string $attribute_type, string $padding ) {
+        protected function generateBody( string $attribute_name, string $attribute_type, ?string $padding ) {
 
             return $padding . $attribute_name . ': this.attr( null )';
         }
 
-        protected function generateRelation( string $attribute_name, string $attribute_type, bool $is_array, string $padding ) {
+        protected function generateRelation( string $attribute_name, string $attribute_type, bool $is_array, ?string $padding ) {
 
             // @todo
             return false;
